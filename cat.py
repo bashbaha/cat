a502889eda37fc2c9fbdb4e948ba11e5942af8d0
huangmingming <dyyzhmm@163.com>
2022-07-07 17:04:48 +0800

    add MyDataset

--- cat.py
+++ cat.py
@@ -9,8 +9,39 @@ def parse_args():
     parser.add_argument('--epochs', default=100, type=int, help='epochs')
 
 class MyDataset(Dataset)：
-    def __init__(self):
+    def __init__(self, mode = 'train'):
         print ("init dataset")
+        self.mode = mode
+        self.datafile = '../data/train_demo.list'
+        self.file_ids = open(self.datafile,'r').readlines()
+        self.width = 500
+        self.height = 64
+ 	self.sample_rate = 8000
+    def __getitem__(self, idx):
+        f,label = self.file_ids[idx].strip().split(" ")
+        wav,sr = torchaudio.load(f,normalize=False)
+        assert sr == self.sample_rate
+        wav = wav / 1.0
+        feature = fbank(wav, dither=1,high_freq=-200, low_freq=64, htk_compat=True,  num_mel_bins=self.height, sample_frequency=self.sample_rate, use_energy=False, window_type='hamming')
+        feature_len = len(feature)
+        if self.mode == "train": #random start pieces
+            rand_start = random.randint(0,feature_len - self.width)
+            print ('debug: rand_start: ' + str(rand_start))
+            feature = feature[rand_start : rand_start + self.width]
+        else: #fixed feature for test
+            feature = feature[0 : self.width]
+
+        #normalize
+        std,mu = torch.std_mean(feature,dim=0)
+        feature = (feature - mu) / (std + 1e-5)
+
+        feature = torch.unsqueeze(feature, dim=0)
+        label = torch.LongTensor([int(label)])
+
+        return feature,label 
+
+    def __len__(self):
+        return len(self.file_ids)
 
 class Generator():
     def __init__(self):
