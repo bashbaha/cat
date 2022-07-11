a3c1684f8a5600ea731166459cf1e77dc599d8c1
huangmingming <dyyzhmm@163.com>
2022-07-11 19:02:58 +0800

    update train process

--- cat.py
+++ cat.py
@@ -2,18 +2,20 @@ from torch.utils.data import Dataset,DataLoader
 from torch import nn
 from tkinter import _flatten
 from torchaudio.compliance.kaldi import fbank
+from torch import optim
 import random
 import numpy as np
 import torchaudio
 import torch
 import math
+import argparse
 
   
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--lr', default=1e-3, type=float, help='learning rate')
-    parser.add_argument('--batch_size', default=16, type=int, help='batch size per GPU')
-    parser.add_argument('--epochs', default=100, type=int, help='epochs')
+    parser.add_argument('--lr', default=2e-1, type=float, help='learning rate')
+    parser.add_argument('--batch_size', default=8, type=int, help='batch size per GPU')
+    parser.add_argument('--epochs', default=10, type=int, help='epochs')
 
 class MyDataset(Dataset):
     def __init__(self, mode = 'train', batch_size = 8):
@@ -211,7 +213,7 @@ class Discriminator_speaker(nn.Module):
         output = self.avgpool(output)
         output = output.squeeze()
         output = output.reshape(output.size(0),(output.size(1)*output.size(2)))
-        output = self.fc(output)
+        #output = self.fc(output)
 
         return output
 
@@ -235,7 +237,7 @@ class FullyConnect_speaker(nn.Module):
     def __init__(self):
         print ("init FullyConnect_speaker")
         super(FullyConnect_speaker,self).__init__() 
-        self.fc == nn.Linear(4 * 512, 512) 
+        self.fc = nn.Linear(4 * 512, 512) 
 
     def forward(self,x):
         return self.fc(x)
@@ -246,13 +248,13 @@ class Discriminator_channel(nn.Module):
         super(Discriminator_channel, self).__init__()
         if GRL:
            self.grl = GRL
-        self.fc1 = nn.Linear()
-        self.fc2 = nn.Linear()
-        self.fc3 = nn.Linear()
-        self.fc4 = nn.Linear()
-        self.fc5 = nn.Linear()
-        self.fc6 = nn.Linear()
-        self.maxpool = nn.MaxPool1d()
+        self.fc1 = nn.Linear(64,64)
+        self.fc2 = nn.Linear(64,64)
+        self.fc3 = nn.Linear(64,64)
+        self.fc4 = nn.Linear(64,64)
+        self.fc5 = nn.Linear(64,64)
+        self.fc6 = nn.Linear(64,64)
+        self.avgpool = nn.AvgPool1d(3, stride=2) #TODO check.
 
     def forward(self, input):
         if getattr(self, "grl", None) is not None:
@@ -263,39 +265,165 @@ class Discriminator_channel(nn.Module):
         output = self.fc4(output)
         output = self.fc5(output)
         output = self.fc6(output)
+        output = self.avgpool(output)
 
         return output
 
-def main(args):
-    num_classes = 251
-    batch_size = args.batch_size
-    lr_init = args.lr
-    epochs = args.epochs
-    
-    D1 = Discriminator_speaker().cuda()
-    FC_D1 = FullyConnect_speaker().cuda()
-    D2 = Discriminator_channel().cuda()
-    G = Generator().cuda()
-    
-    Class_CrossEntropyLoss = nn.CrossEntropyLoss()
-    AdversarialLoss = nn.BCELoss()
-    
-    for epoch in range(epochs):
-        train_one_epoch(train_dataloader, G, D1, FC_D1, D2, Class_CrossEntropyLoss, AdversarialLoss, optimizer_G, optimizer_D1, optimizer_D2, epoch, batch_size, args)
+class TripleLoss(nn.Module):
+    def __init__(self, margin=0.3):
+        super(TripleLoss, self).__init__()
+        self.margin = margin # 阈值
+        self.rank_loss = nn.MarginRankingLoss(margin=margin)
+        self.cos = nn.CosineSimilarity(dim=1, eps=1e-6)
+        self.tripletloss = nn.TripletMarginLoss(margin=1.0, reduction='sum')
+
+    def forward(self, inputs, labels, norm=False):      
+        dist_mat = self.cosine_dist(inputs, inputs, norm=norm)  # 距离矩阵,越大越相似。
+        dist_ap, dist_an = self.hard_sample(dist_mat, labels) # 取出每个anchor对应的hard sample.
+        loss = self.tripletloss(inputs,dist_ap,dist_an)
+        
+        return loss
+
+    @staticmethod
+    def hard_sample( dist_mat, labels, ):
+        # 距离矩阵的尺寸是 (batch_size, batch_size)
+        assert len(dist_mat.size()) == 2
+        assert dist_mat.size(0) == dist_mat.size(1)
+        N = dist_mat.size(0)
+
+        # 选出所有正负样本对
+        is_pos = labels.expand(N, N).eq(labels.expand(N, N).t()) # 两两组合， 取label相同的a-p
+        is_neg = labels.expand(N, N).ne(labels.expand(N, N).t()) # 两两组合， 取label不同的a-n
+
+        list_ap, list_an = [], []
+        # 取出所有正样本对和负样本对的距离值
+        for i in range(N):
+            list_ap.append( dist_mat[i][is_pos[i]].min().unsqueeze(0) )  #hard: 相同标签，选择距离小的。
+            list_an.append( dist_mat[i][is_neg[i]].max().unsqueeze(0) )  #hard: 不同标签，选择距离大的。
+
+        dist_ap = torch.cat(list_ap)  # 将list里的tensor拼接成新的tensor
+        dist_an = torch.cat(list_an)
+
+        return dist_ap, dist_an
+
+    @staticmethod
+    def normalize(x, axis=1):
+        x = 1.0*x / (torch.norm(x, 2, axis, keepdim=True) + 1e-12)
+        return x
+
+    @staticmethod
+    def euclidean_dist(x, y, norm=True):
+        if norm: #when vector is normalized, euclidean_dist is almost equal to cosine dist.
+            x = self.normalize(x)
+            y = self.normalize(y)
+        m, n = x.size(0), y.size(0)
+        xx = torch.pow(x, 2).sum(dim=1, keepdim=True).expand(m, n)
+        yy = torch.pow(y, 2).sum(dim=1, keepdim=True).expand(n, m).t()
+        dist = xx + yy # 任意的两个样本组合， 求第二范数后求和 x^2 + y^2
+        dist.addmm_( 1, -2, x, y.t() ) # (x-y)^2 = x^2 + y^2 - 2xy
+        dist = dist.clamp(min=1e-12).sqrt() #开方
+        return dist
+
+    @staticmethod
+    def cosine_dist(x, y):
+        x = self.normalize(x)
+        y = self.normalize(y)
+        dist = torch.mm(x,y) #cosine distance because x,y are normalized.
+        return dist
+
+
 
 def normalize(x, axis=1):
     x = 1.0*x / (torch.norm(x, 2, axis, keepdim=True) + 1e-12)
     return x
 
-def train_one_epoch(train_dataloader, G, D1, FC_D1, D2, Class_CrossEntropyLoss, AdversarialLoss, optimizer_G, optimizer_D1, optimizer_D2, epoch, batch_size, args):
+def main(args):
+    num_classes = 20
+    batch_size = 8
+    lr_init = 0.02
+    epochs = 10
+    input_size = 64 
+    hidden_size = 128
+    projection_size = 64
+    speaker_classes = 512
+    
+    D1 = Discriminator_speaker(speaker_classes=num_classes).cuda()
+    FC_D1 = FullyConnect_speaker().cuda()
+    D2 = Discriminator_channel().cuda()
+    G = Generator(input_size=input_size, hidden_size=hidden_size, projection_size=projection_size).cuda()
+    
+    Class_CrossEntropyLoss = nn.CrossEntropyLoss()
+    AdversarialLoss = nn.BCELoss()
+    Triple_loss = TripleLoss()
+
+    optimizer_D1G = optim.SGD([ {'params': D1.parameters()}, {'params': FC_D1.parameters()}, {'params': G.parameters(), 'lr': 1e-3} ], lr=2e-1, momentum=0.9)
+    optimizer_D2 = optim.SGD([ {'params': D2.parameters()}, ], lr=2e-1, momentum=0.9)
+
+
+    train_dataset = MyDataset(mode='train')
+    train_dataloader = DataLoader(train_dataset,batch_size = 1, shuffle=False, drop_last=True, num_workers=5 )
+    
+    for epoch in range(epochs):
+        train_one_epoch(train_dataloader, G, D1, FC_D1, D2, Class_CrossEntropyLoss, AdversarialLoss,Triple_loss,  optimizer_D1G, optimizer_D2, epoch, batch_size, args)
+
+
+def train_one_epoch(train_dataloader, G, D1, FC_D1, D2, Class_CrossEntropyLoss, AdversarialLoss, Triple_loss,  optimizer_D1G, optimizer_D2, epoch, batch_size, args):
     for batch_id,(x,y) in enumerate(train_dataloader):
+
+        optimizer_D1G.zero_grad()
+        optimizer_D2.zero_grad()
+
         x = x.cuda()
         y = y.cuda()
+        input_size = 64 
+        hidden_size = 128
+        projection_size = 64
+        speaker_classes = 512
         
-        generator_feature = G(x)
-        spk_feature = D1(generator_feature)
-        pred_speakerid = FC_D1(spk_feature)
+        #example input & output
+        input = torch.randn(8, 500, 64, 1)
+        input = input.squeeze(dim=-1)
+        input = input.transpose(dim0=0, dim1=1)
+        hx = input.new_zeros(input.size(1), projection_size, requires_grad=False)
+        cx = input.new_zeros(input.size(1), hidden_size, requires_grad=False)
+        state = [hx, cx]
+        print (x.shape)
+        generator_feature = G(x,state)
+        print (generator_feature.shape)
+        spk_embedding = D1(generator_feature)
+        pred_speakerid = FC_D1(spk_embedding)
         pred_channel = D2(generator_feature)
+        
+ 
+        #train G,D1
+        G.train()
+        D1.train()
+        D2.eval()
+        #compute loss
+        Ls = Class_CrossEntropyLoss(pred_speakerid,y)
+        Lt = Triple_loss(spk_embedding, y)
+        L_d1 = Ls + Lt
+        L_d1.backward()
+        #update gradients.  TODO:MIN?
+        optimizer_D1G.step()
+
+        #train D2
+        G.eval()
+        D1.eval()
+        D2.train()
+        #get channel_label
+        channel_label = []
+        for i in y:
+            if i.item() < 10:
+                channel_label.append(0)    
+            else:
+                channel_label.append(1)    
+        channel_label = torch.from_numpy(np.array(channel_label))
+        L_d2 = Class_CrossEntropyLoss(pred_channel,channel_label)
+        L_d2.backward()
+        #update gradients. TODO:max?
+        optimizer_D2.step()
+        
 
         # example input & output
         # input = torch.randn(2, 500, 64, 1)
@@ -311,20 +439,12 @@ def train_one_epoch(train_dataloader, G, D1, FC_D1, D2, Class_CrossEntropyLoss, 
         # D = Discriminator_speaker(speaker_classes=speaker_classes)
         # output = D(output) 
 
-        Ls = Class_CrossEntropyLoss(pred_speakerid,y)
-        #TODO: spk_feature needs to be normalized for cosine distance, hmm think.
-
-        #Lt =  
-                
-        #compute loss
-        #update gradients
-        #save log & model
         pass
     
        
 if __name__ == '__main__':
-    #args = parse_args()
-    #main(args)
-    dataset = MyDataset()
-    dataset.__getitem__(0)
+    args = parse_args()
+    main(args)
+    #dataset = MyDataset()
+    #dataset.__getitem__(0)
 
